970afb665cb2a20889bc66021685626222d69978
Abhi5063 <abhiupadhyay5063@gmail.com>
2025-09-17 23:47:28 +0530

    th

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+1;
+
+2;
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
