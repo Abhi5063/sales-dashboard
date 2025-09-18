d917687929b1e3193bceef951d30cff94d4baae9
Abhi5063 <abhiupadhyay5063@gmail.com>
2025-09-18 08:22:33 +0530

    .

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Input: React.FC<InputProps> = (props) => {
+export const Input: React.FC<InputProps> = ({ className, ...props }) => {
   return (
     <input
       {...props}
-      className="border rounded-lg p-2 w-full focus:outline-none focus:ring focus:ring-blue-400"
+      className={`border rounded-lg p-2 w-full focus:outline-none focus:ring focus:ring-blue-400 ${
+        className ?? ""
+      }`}
     />
   );
 };
